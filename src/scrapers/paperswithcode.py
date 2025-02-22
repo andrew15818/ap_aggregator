515025ac5d7d34489f9c451052c868411c1b775f
Andres Ponce <andreseeponce@gmail.com>
2025-02-22 16:50:54 +0800

    Feat: Add initial paper link, paper name scraping

--- src/scrapers/paperswithcode.py
+++ src/scrapers/paperswithcode.py
@@ -1,21 +1,40 @@
 import requests  # type: ignore
 from bs4 import BeautifulSoup
 
-from src.scrapers.base import BaseScraper
+from src.scrapers.base import BaseScraper, BaseResponse, BaseResponses
 
 
-class PapersWithCode(BaseScraper):
+class PapersWithCodeScraper(BaseScraper):
     def __init__(self, url: str):
-        super(PapersWithCode, self).__init__(url)
+        super(PapersWithCodeScraper, self).__init__(url)
         self.url = url
-        pass
+        self.html = BeautifulSoup(
+            requests.get(self.url).text,
+            "html.parser",
+        )
 
-    def scrape(self):
-        html = BeautifulSoup(requests.get(self.url).text, "html.parser")
-        print(html)
-        pass
+    def scrape(self) -> BaseResponses:
+        """
+        Scrape and format the response from PapersWithCode HTML.
+        """
+        try:
+            papers_html = self.html.find(class_="home-page").find_all(
+                class_="item-content"
+            )
+            for paper in papers_html:
+                title_info = paper.find("h1").find("a")
+                title = title_info.contents[0]
+                link = (
+                    self.url + title_info["href"]
+                )  # Only link relative to homepage is returned
+                print(f"Title: {title}, link: {link}")
+
+                break
+            return BaseResponses([BaseResponse()])
+        except Exception as e:
+            raise Exception(f"Could not parse PapersWithCode with error {e}")
 
 
 if __name__ == "__main__":
-    test = PapersWithCode(url="https://www.paperswithcode.com")
+    test = PapersWithCodeScraper(url="https://www.paperswithcode.com")
     test.scrape()
