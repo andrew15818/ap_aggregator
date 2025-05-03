018d5c82507df4c9dd5f2f00231741ebace8e2aa
Andres Ponce <andreseeponce@gmail.com>
2025-05-04 00:28:36 +0800

    Fix: Ignore typerequest operation types

--- src/scrapers/arxiv.py
+++ src/scrapers/arxiv.py
@@ -1,12 +1,11 @@
-import requests
+import requests  # type: ignore
 from src.scrapers.base import BaseResponse, BaseResponses, BaseScraper
-from bs4 import BeautifulSoup
-import urllib.request as libreq
-import feedparser
+import urllib.request as libreq  # type: ignore
+import feedparser  # type: ignore
 
 
 class ArxivScraper(BaseScraper):
-    def __init__(self, url: str):
+    def __init__(self, url: str = "http://export.arxiv.org/api/query?"):
         super(ArxivScraper, self).__init__(url)
         self.url = url
         self.query = "search_query=cat:cs.CV+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.AI+OR+cat:cs.NE+OR+cat:cs.RO"  # Query for these categories
@@ -19,10 +18,10 @@ class ArxivScraper(BaseScraper):
             self.final_url,
         )
         with libreq.urlopen(self.final_url) as url:
-            response = url.read()
+            response = url.read()  # type: ignore
 
-        if url.status == 200:
-            self.feed = feedparser.parse(response)
+        if url.status == 200:  # type: ignore
+            self.feed = feedparser.parse(response)  # type: ignore
         else:
             print(f"Error querying {self.final_url}")
 
@@ -30,17 +29,18 @@ class ArxivScraper(BaseScraper):
         if not self.feed:
             raise Exception(f"Could not parse url {self.final_url}")
 
+        responses = []
         for item in self.feed.entries:
             response = BaseResponse(
-                title=item["title"],
+                title=item["title"].replace("\n", ""),
                 content=item.title_detail["value"],
                 link=item["link"],
                 authors=[n["name"] for n in item.authors],
             )
             print(response.title)
-            break
+            responses.append(response)
 
-        return BaseResponses(contents=[BaseResponse()])
+        return BaseResponses(contents=responses)
 
 
 if __name__ == "__main__":
