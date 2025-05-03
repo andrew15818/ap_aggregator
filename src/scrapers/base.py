9e050ae44f1cce81cbb1017078912da385013620
Andres Ponce <andreseeponce@gmail.com>
2025-05-04 00:29:41 +0800

    Feat: add get_items function to format baseresponse items as iterable

--- src/scrapers/base.py
+++ src/scrapers/base.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from pydantic import BaseModel
 
 
@@ -12,6 +12,12 @@ class BaseResponse(BaseModel):
     link: str = "https://example.com"
     authors: list = ["Andres Ponce"]
 
+    def __str__(self) -> str:
+        return f"Title: {self.title} Link: {self.link} Authors: {self.authors} Content: {self.content}"
+
+    def get_items(self) -> list[str | list[str]]:
+        return [self.title, self.content, self.link]  # , self.authors]
+
 
 class BaseResponses(BaseModel):
     """
@@ -20,11 +26,15 @@ class BaseResponses(BaseModel):
 
     contents: list[BaseResponse]
 
+    def get_contents(self) -> list[BaseResponse]:
+        return self.contents
+
 
 class BaseScraper(ABC):
     def __init__(self, url: str):
         self.url = url
 
+    @abstractmethod
     def scrape(self) -> BaseResponses:
         """
         Scrape the website for relevant article links,
